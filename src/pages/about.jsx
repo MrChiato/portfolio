5547e08814057376d628ead486cfd22b38ac96d1
MrChiato <mattias@raver.dk>
2022-11-02 01:47:44 +0100

    Updated CSS so it scales on phone and bigger screens, hopefully.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,9 @@ export function About(){
         <div id="about-menu">
             <span className='plain-text'>&lt;div id=<span className='white-text'>"about"</span>&gt;</span>
             <h2 className="about-header">&lt;h2&gt;About Mattias&lt;/h2&gt;</h2>
-            <p className="about-text">&lt;p&gt;I am a CS major, currently working on expanding my knowledge within Front-end development, with hopes of landing my first junior developer job soon, so I can learn and improve on my programming skills.&lt;/p&gt;</p>
+            <div className="aboutTextDiv">
+                <p className="about-text">&lt;p&gt;I am a CS major, currently working on expanding my knowledge within Front-end development, with hopes of landing my first junior developer job soon, so I can learn and improve on my programming skills.&lt;/p&gt;</p>
+            </div>
             <a className="mail-text" href = "mailto: mattias@raver.dk"><span className='mail-tag'>&lt;a&gt;</span>Send Mail<span className='mail-tag'>&lt;/a&gt;</span></a>
             
             <div id="menu-items">
